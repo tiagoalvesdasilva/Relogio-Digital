cbfe90aecc30506df7326b98daa380474b0dae37
Tiago Alves <tiago.alvessilva67@gmail.com>
2023-02-14 21:59:46 -0300

    Modificando os arquivos index e script

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function time () {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let txt = h+':'+ m+':'+s;
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos')
 
-    document.querySelector('.relogio').innerHTML = txt;
+function time (){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let m = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+    if (hr < 10 ){
+        hr = '0'+ hr
+    }
+     if (m < 10){
+        m = '0' + m;
+    } 
+    if (s < 10 ){
+        s = '0' + s
+    }
+    horas.textContent = hr
+    minutos.textContent = m
+    segundos.textContent = s
 }
 let timer = setInterval(time, 1000);
